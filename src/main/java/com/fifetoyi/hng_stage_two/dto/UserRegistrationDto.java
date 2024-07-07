52233f7228fc3684bbe75f569e90a2f613a9efbf
Fifetoyi <fifetoyi@gmail.com>
2024-07-07 11:30:20 +0100

    Add user registration and login dto

---
+++ src/main/java/com/fifetoyi/hng_stage_two/dto/UserRegistrationDto.java
@@ -0,0 +1,55 @@
+package com.fifetoyi.hng_stage_two.dto;
+
+import javax.validation.constraints.NotNull;
+
+public class UserRegistrationDto {
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+    private String phone;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
