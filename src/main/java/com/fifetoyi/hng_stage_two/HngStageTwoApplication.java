f8fdfe7caa7cb57775616649dc82ed5f350fb4cc
Fifetoyi <fifetoyi@gmail.com>
2024-07-07 18:40:17 +0100

    Add entity scan

--- src/main/java/com/fifetoyi/hng_stage_two/HngStageTwoApplication.java
+++ src/main/java/com/fifetoyi/hng_stage_two/HngStageTwoApplication.java
@@ -2,9 +2,12 @@ package com.fifetoyi.hng_stage_two;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EntityScan("com.fifetoyi.hng_stage_two.model")
 public class HngStageTwoApplication {
 
     public static void main(final String[] args) {
