005a3ec6318d9adf12f426f36348e369e831b6e7
Fifetoyi <fifetoyi@gmail.com>
2024-07-07 12:59:32 +0100

    Configure spring security

---
+++ src/main/java/com/fifetoyi/hng_stage_two/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.fifetoyi.hng_stage_two.service;
+
+import com.fifetoyi.hng_stage_two.model.User;
+import com.fifetoyi.hng_stage_two.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userOpt = userRepository.findByEmail(email);
+
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+    }
+}
+
